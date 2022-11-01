c402b04d69cebd9182675f6ee269e42e20abaab0
MOHD_ALLAHUDDIN <MOHDALLAHUDDIN44@gmail.com>
2022-11-01 10:30:54 +0530

    validators implemented

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public loginForm:FormGroup = new FormGroup(
+    {
+      email: new FormControl(),
+      password: new FormControl()
+    }
+  )
+  constructor(private _loginService:LoginService, private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  login (){
+    this._loginService.login(this.loginForm.value).subscribe(
+      (data:any)=>{
+        sessionStorage.setItem("my-app-token",data.token)
+        this.router.navigateByUrl("/dashboard");
+      },
+      (err:any)=>{
+        alert("Internal credentials");
+      }
+    )
+  }
 }
